4ed86d1d0a7e71e37efc47b88b17fbaf5b8c112e
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-01-21 08:00:01 +0100

    implemented splash screen

---
+++ app/src/main/java/com/thefadaapp/fadapharmacy/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.thefadaapp.fadapharmacy.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val FadaPrimary = Color(0xFF3C9386)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val FadaAccentLight = Color(0xFFFAAB54)
+
+val FadaPrimaryDark = Color(0xFFAAAAAA)
+val PurpleGrey40 = Color(0xFF625b71)
+val FadaAccentDark = Color(0xFF7D5260)
